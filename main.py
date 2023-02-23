9053ba93669827fc052a9d21db89ef43def7d18d
Alex1iv <ivanov.politehnik@gmail.com>
2023-02-23 18:04:36 +0600

    upd streamlit app

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ if __name__ == "__main__":
     print('The program returns prediction by given data')
     #start_streamlit()
     
-    sys.argv = ["streamlit", "run", "utils/application.py"]
+    sys.argv = ["streamlit", "run", "application.py"]
     
     sys.exit(stcli.main())
     

dd2d8b7c3d3eafb107f0972a5418a9ae87f0e61c
Alex1iv <ivanov.politehnik@gmail.com>
2023-03-10 17:36:58 +0600

    add drop redundant columns

--- application.py
+++ application.py
@@ -144,14 +144,13 @@ limits = pd.DataFrame({
 
 if test_mode == 'Test_yes':
     test_df = pd.DataFrame(data_yes)
-    #test_df['duration'] = (test_df['duration']-(limits['duration'].min()))/(limits['duration'].max()-(limits['duration'].min()))
+
     
 elif test_mode == 'Test_no':
-    test_df = pd.DataFrame(data_no) #.items()
-    #test_df['duration'] = (test_df['duration']-(limits['duration'].min()))/(limits['duration'].max()-(limits['duration'].min()))
-else:
-    test_df = pd.DataFrame(data, index=[0]) #.items()
+    test_df = pd.DataFrame(data_no) 
 
+else:
+    test_df = pd.DataFrame(data, index=[0]) 
 
 st.write(test_df)
 print(test_df)
@@ -178,44 +177,25 @@ for i in ['success', 'unknown']:
 for i in ['cellular', 'unknown']:
     test_df[f'contact_{i}'] = test_df['contact'].apply(lambda x: 1 if x==i else 0)
 
+
 test_df.drop(['poutcome', 'contact_cellular'], axis=1, inplace=True)
+
+for i in test_df.columns:
+    col_list = ['poutcome_success', 'balance', 'contact_unknown', 'age','pdays']
+    if i not in col_list:
+       test_df.drop(i, axis=1, inplace=True) 
+    
 test_df = test_df[['poutcome_success', 'balance', 'contact_unknown', 'age','pdays']]
 
 y_pred, y_prob= model.predict(test_df), model.predict_proba(test_df)[:,1]
 
 
-# for j in ['balance',  'pdays', 'previous', 'campaign']: #'duration',
-#     test_df[j] = (test_df[j]-(limits[j].min()))/(limits[j].max()-(limits[j].min()))
 
-# """
-# for i in ['mar', 'may', 'oct', 'sep']:
-#     test_df[f'month_{i}'] = test_df['month'].apply(lambda x: 1 if x==i else 0)
-
-# test_df['contact_cellular'] = test_df['contact'].apply(lambda x: 1 if x=='cellular' else 0)
-
-# test_df['contact_unknown'] = test_df['contact'].apply(lambda x: 1 if x=='unknown' else 0) 
-
-# for i in ['success', 'unknown']:
-#     test_df[f'poutcome_{i}'] = test_df['poutcome'].apply(lambda x: 1 if x==i else 0)
-
-# for i in ['housing', 'loan']:
-#     test_df[i] = test_df[i].apply(lambda x: 1 if x=='yes' else 0)
-# """ 
-
-# #test_df['housing'] = test_df['housing'].apply(lambda x: 1 if x=='yes' else 0)
-
-# #test_df['age_group_60+'] = test_df['age'].apply(lambda x: 1 if 60<=x<=95 else 0)
-# #test_df = test_df.drop(labels=['age']) # for series
-
-# test_df['age_group'] = pd.cut(test_df['age'], [0,30,40,50,60,9999], labels = ['<30','30-40','40-50','50-60','60+'])
-
-# df_encoded = pd.get_dummies(test_df)
 
 
 # df_encoded.drop(['job', 'marital','default', 'day', 'contact', 'month','poutcome'], axis=1, inplace=True) #'age_group','education', 'loan','deposit'
 
-# # Set columns order 
-# test_df = test_df[['default', 'housing', 'loan', 'campaign', 'pdays', 'previous','job_admin.', 'job_blue-collar', 'job_entrepreneur', 'job_housemaid','job_management', 'job_retired', 'job_self-employed', 'job_services','job_student', 'job_technician', 'job_unemployed', 'marital_divorced','marital_married', 'marital_single', 'education_primary','education_secondary', 'education_tertiary', 'contact_cellular','contact_telephone', 'contact_unknown', 'month_apr', 'month_aug','month_dec', 'month_feb', 'month_jan', 'month_jul', 'month_jun','month_mar', 'month_may', 'month_nov', 'month_oct', 'month_sep','poutcome_failure', 'poutcome_other', 'poutcome_success','poutcome_unknown', 'age_group_<30', 'age_group_30-40','age_group_40-50', 'age_group_50-60', 'age_group_60+', 'day_of_week_Mon', 'day_of_week_Tue', 'day_of_week_wed', 'day_of_week_Thu', 'day_of_week_Fri', 'day_of_week_Sat','day_of_week_Sun', 'age', 'balance']]
+
 
 
 # Prediction of probabilities:
